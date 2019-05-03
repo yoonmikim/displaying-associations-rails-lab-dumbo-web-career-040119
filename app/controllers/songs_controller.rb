ad101c4484cdb0645798e6e11745128fe4775fe7
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-03 23:55:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,17 @@
 class SongsController < ApplicationController
+  
+  before_action :get_song, only: [:show, :edit, :update]
+  
   def index
+    @songs = Song.all
   end
 
   def show
+    
   end
 
   def new
+    @song = Song.new
   end
 
   def create
@@ -45,6 +51,10 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title)
+  end
+  
+  def get_song 
+    @song = Song.find(params[:id])
   end
 end
 
